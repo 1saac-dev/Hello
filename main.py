903f2fac3b79600638de08399847058efe4ee1e4
1saac <1saac.dev@protonmail.com>
2021-09-16 20:25:51 -0400

    changed main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-x = "Hello World!"
+calc_to_unit = 24
+name_of_unit = "hours"
 
-print(x)
+
+def days_to_units(num_of_days):
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calc_to_unit} {name_of_unit}"
+    else:
+        return "Please, enter a positive number."
+
+
+user_input = input("Hey user, enter a number of days.\n")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
